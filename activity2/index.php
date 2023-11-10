4ddd6adb9a0ef580f93ba32b37f44736bebc4782
raianrandell <131576617+raianrandell@users.noreply.github.com>
2023-11-10 17:18:19 +0800

    add icon

--- activity2/index.php
+++ activity2/index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="activity2/agecalculator.png" type="image/png">
     <title>Age Calculator</title>
     <style>
 * {
